2a6cd254447e82d7212677b6f3cfd78ed2d7697e
sunyanguo <sunyanguo@lvmama.com>
2016-12-30 14:34:39 +0800

    DOCUMENTING

--- lib/PodfileTool.rb
+++ lib/PodfileTool.rb
@@ -3,13 +3,17 @@ require 'json'
 require 'cocoapods-core'
 
 class PodfileTool
+  # parse Cocoapods Podfile to json
+  #
+  # Example:
+  #   >> PodfileTool.outputJson("/path_of_Podfile")
+  #   => {json}
+  #
+  # Arguments:
+  #   path: (path_of_Podfile)
   def self.outputJson(path)
     podfile_hash = Pod::Podfile.from_file(path).to_hash
     podfile_json = JSON.pretty_generate(podfile_hash)
     puts podfile_json
-  end
-
-  def self.hi
-    puts "Hello world!"
   end
 end
