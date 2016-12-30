ece4dba9c745ac838350e05030c36c2c66caac9e
sunyanguo <sunyanguo@lvmama.com>
2016-12-30 14:54:18 +0800

    Commit add bin

--- lib/PodfileTool.rb
+++ lib/PodfileTool.rb
@@ -11,9 +11,27 @@ class PodfileTool
   #
   # Arguments:
   #   path: (path_of_Podfile)
+  # output to file name Podfile.json
+  # Example2:
+  #   >> PodfileTool.outputJsonFile("/path_of_Podfile")
+  #   => {json}
+  #
+  # Arguments:
+  #   path: (path_of_Podfile)
   def self.outputJson(path)
     podfile_hash = Pod::Podfile.from_file(path).to_hash
     podfile_json = JSON.pretty_generate(podfile_hash)
     puts podfile_json
+  end
+
+  def self.outputJsonFile(path)
+    podfile_hash = Pod::Podfile.from_file(path).to_hash
+    podfile_json = JSON.pretty_generate(podfile_hash)
+    aFile = File.new(Dir.pwd+"/Podfile.json", "w")
+    if aFile
+        aFile.syswrite(podfile_json)
+    else
+        puts "Unable to open file!"
+    end
   end
 end
